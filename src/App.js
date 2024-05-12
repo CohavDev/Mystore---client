a4597918c44f30a01715f0a5f5c1d288b2ddf919
CohavDev <shaulspam@gmail.com>
2024-05-12 11:28:46 +0300

    search box added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { Box, Button } from "@mui/joy";
 import SearchBox from "./components/searchBox";
 import StoreTable from "./components/storeTable";
 import logo from "./logo.svg";
+import { Add, Save } from "@mui/icons-material";
 // import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
+        <div style={{ margin: 25 }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <h2>Store Items</h2>
+            <Box
+              sx={{
+                display: "flex",
+                alignContent: "center",
+                gap: 2,
+                flexWrap: "wrap",
+              }}
+            >
+              <Button startDecorator={<Add />}>Add Item</Button>
+              <Button disabled startDecorator={<Save />}>
+                Save
+              </Button>
+              <Button>Exit & Cancel</Button>
+            </Box>
+          </Box>
           <SearchBox />
           <StoreTable />
         </div>
--- src/components/searchBox.jsx
+++ src/components/searchBox.jsx
@@ -3,11 +3,18 @@ import { Search } from "@mui/icons-material";
 
 export default function SearchBox() {
   return (
-    <Box display="flex" justifyContent="center">
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        padding: 2,
+      }}
+    >
       <Input
         placeholder="Search for a name"
         startDecorator={<Search />}
         endDecorator={<Button>Search</Button>}
+        sx={{ width: "30%" }}
       />
     </Box>
   );
